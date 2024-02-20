7c12cbb25ea8d143739cab1ce0ab5f7617426de7
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-02-20 19:43:18 +0100

    Declared constants and variables to be used

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//listener added to wait for DOM to load before loading script file
 document.addEventListener("DOMContentLoaded", () => {
-    
+
+//assigned constants to elements that will be affected by following code, declared starting values for X and Y
+
+    const btn = document.querySelector('button');
+    const container = document.querySelector('.container');
+    let x = 16;
+    let y = 16;
+
+//function creates initial 16x16 divs    
+    function drawStart(){
+
+    }
 })
